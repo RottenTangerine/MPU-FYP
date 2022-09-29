c82299230227182f574a2c9c1a3062974a3ce787
Eden <xiang_xuan@qq.com>
2022-09-29 22:36:28 +0800

    :sparkles: feat: add config file

---
+++ Code/config.py
@@ -0,0 +1,35 @@
+import argparse
+
+def load_config():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--path',
+                        type=str,
+                        default='/Dataset/MSRA-TD500',
+                        help='dataset path')
+
+
+    # parameters
+    parser.add_argument('-e',
+                        '--epochs',
+                        type=int,
+                        default=20,
+                        help='default training epochs')
+    parser.add_argument('-l',
+                        '--lr',
+                        type=float,
+                        default=1e-3,
+                        help='initial learning rate')
+    parser.add_argument('-b',
+                        '--batch_size',
+                        type=int,
+                        default=32,
+                        help='batch size')
+
+    # model
+    parser.add_argument('--cuda',
+                        type=bool,
+                        default=False,
+                        help='Use GPU training')
+
+    return parser.parse_args()
