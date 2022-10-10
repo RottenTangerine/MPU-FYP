67df7d25318c43e6f6e064d2e92653e36fc23659
Eden <xiang_xuan@qq.com>
2022-10-11 02:35:36 +0800

    :construction: feat: dataset loading  part (half finished)

--- Code/dataset.py
+++ Code/dataset.py
@@ -1,58 +1,53 @@
 import os
 from PIL import Image
-
 from torch.utils.data import Dataset
 import torchvision.transforms as T
 
+import matplotlib.pyplot as plt
+
+from parse_gt import parse_gt
+from CTPN_utils import draw_gt_boxes
+
+
+
 class ICPRDataset(Dataset):
-    def __init__(self, path):
+    def __init__(self, args):
         """
         load ICPR dataset
         :param path: path of the dataset directory, contains image_train and txt_train directories
         """
-        self.path = path
-        self.data_path = os.path.join(path, 'image_train')
-        self.label_path = os.path.join(path, 'txt_train')
-        self.img_names = os.listdir(os.path.join(path, 'image_train'))
+        self.path = args.path
+        self.data_path = os.path.join(self.path, 'image_train')
+        self.label_path = os.path.join(self.path, 'txt_train')
+        self.img_names = os.listdir(os.path.join(self.path, 'image_train'))
 
     def __len__(self):
         return len(self.img_names)
 
 
-    def parse_label(self, path):
-        label_lists = []
-        with open(path, encoding='UTF-8') as f:
-            lines = f.readlines()
-            for line in lines:
-                label_list = line.split(',')[:8]
-                label_lists.append(label_list)
-        return label_lists
-
-
     def __getitem__(self, index):
-        img_name = self.img_names[index]
-        print(img_name)
-        i = img_name.split('.')[:-1]
-        img_name = '.'.join(i)
-        # img
-        img_path = os.path.join(self.data_path, self.img_names[index])
-        img = Image.open(img_path)
-        # h, w = img.size
-        # rescale_fac = max(img.size) / 1600
-        # if rescale_fac > 1.0:
-        #     trans = T.Compose([T.Resize((int(h / rescale_fac), int(w / rescale_fac))),
-        #                        T.ToTensor()])
+        try:
+            img_name = self.img_names[index]
+            i = img_name.split('.')[:-1]
+            img_name = '.'.join(i)
+            del i
+            # img
+            img_path = os.path.join(self.data_path, self.img_names[index])
+            img = Image.open(img_path)
+
+            # label
+            label_path = os.path.join(self.label_path, f'{img_name}.txt')
+            gtbox = parse_gt(label_path)
+        except:
+            print('fail to load image, use default image')
+            return self[0]
+
+        draw_gt_boxes(img, gtbox)
         trans = T.ToTensor()
         img = trans(img)
 
-        # label
-        label_path = os.path.join(self.label_path, f'{img_name}.txt')
-        gtbox = self.parse_label(label_path)
 
-        # clip image:
-
-
-
+        print(gtbox)
         return img, gtbox
 
 
@@ -60,11 +55,15 @@ class ICPRDataset(Dataset):
 
 if __name__ == '__main__':
     import numpy as np
-    import matplotlib.pyplot as plt
     from torch.utils.data import DataLoader
+    from config import load_config
 
-    dataset = ICPRDataset('Dataset/ICPR2018')
-    dataset = DataLoader(dataset, batch_size=1)
+    args = load_config()
+
+    img_path = os.path.join(args.path, 'image_train')
+
+    dataset = ICPRDataset(args)
+    dataset = DataLoader(dataset, batch_size=1, shuffle=True)
     data, label = next(iter(dataset))
     data = np.array(data)
     data = data.squeeze()
@@ -72,5 +71,6 @@ if __name__ == '__main__':
     plt.imshow(data)
     plt.show()
 
-    print(label)
+
+
 
