593e98f70791306146701ea20aa1a2f63566a954
Eden Xiang <xiang_xuan@qq.com>
2022-10-20 02:21:40 +0800

    :bug: fix: fix gt_label unmatch issue

--- Code/Detection/dataset.py
+++ Code/Detection/dataset.py
@@ -4,13 +4,9 @@ import cv2
 import torch
 from torch.utils.data import Dataset
 import numpy as np
-import torchvision.transforms as T
-
-import matplotlib.pyplot as plt
 
 from parse_gt import parse_gt
 from CTPN_utils import draw_gt_boxes, gen_basic_anchor, cal_rpn
-
 
 
 class ICPRDataset(Dataset):
@@ -28,7 +24,6 @@ class ICPRDataset(Dataset):
     def __len__(self):
         return len(self.img_names)
 
-
     def __getitem__(self, index):
         img_name = self.img_names[index]
         i = img_name.split('.')[:-1]
@@ -42,20 +37,19 @@ class ICPRDataset(Dataset):
         if img is not None:
             # rescale img
             h, w, c = img.shape
-            rescale_fac = max(h, 2) / 800
+            rescale_fac = max(h, w) / 1600
             if rescale_fac > 1.0:
                 h = int(h / rescale_fac)
-                w = int(h / rescale_fac)
+                w = int(w / rescale_fac)
                 img = cv2.resize(img, (w, h))
-
 
             # label
             label_path = os.path.join(self.label_path, f'{img_name}.txt')
             gtbox = parse_gt(label_path, rescale_fac)
+            # draw_gt_boxes(img, gtbox)
 
             [cls, regr], base_anchors = cal_rpn(self.args, (h, w), (int(h / 16), int(w / 16)), 16, gtbox)
 
-            # cv2.imwrite('debug/{}'.format(img_name),debug_img)
             m_img = img - self.args.IMAGE_MEAN
 
             regr = np.hstack([cls.reshape(cls.shape[0], 1), regr])
@@ -74,8 +68,6 @@ class ICPRDataset(Dataset):
             return self[0]
 
 
-
-
 if __name__ == '__main__':
     import numpy as np
     from torch.utils.data import DataLoader
@@ -88,7 +80,3 @@ if __name__ == '__main__':
     dataset = DataLoader(dataset, batch_size=1, shuffle=True)
     imgs, classes, regrs = next(iter(dataset))
     ic(classes.shape, regrs.shape)
-
-
-
-
